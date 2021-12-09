373eec05addd6e5ba68c694e9b2817f9adc9c508
Gustavo Rodriguez <gustavo.rodriguezv30@gmail.com>
2021-12-08 20:42:59 -0400

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace sandia
+{
+    class Program
+    {
+
+        //programa con entrada/salida estandar
+        static void Main(string[] args)
+        {
+            int peso;
+
+            peso = Int32.Parse(Console.ReadLine());
+            
+            if (peso % 2 ==0 && peso !=2) {
+                Console.WriteLine("YES");
+
+            }
+            else {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
